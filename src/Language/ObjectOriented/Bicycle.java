2d99ba56cb1a68eba5d9bf0aaf44a17bf2b5c4fd
sirlearning <zhengnda@outlook.com>
2024-09-05 13:13:03 +0800

    1

--- src/Language/ObjectOriented/Bicycle.java
+++
@@ -1,9 +0,0 @@
-package Language.ObjectOriented;
-public interface Bicycle {
-    // wheel revolutiions per minute
-    public void changeCadence(int newValue);
-    public void changeGear(int newValue);
-    public void speedUp(int increment);
-    public void applyBrakes(int decrement);
-    public void printStates();
-}
