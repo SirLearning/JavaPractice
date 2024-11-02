c1f7a9166a78ae0def26a803f77140e3a3a900f2
sirlearning <zhengnda@outlook.com>
2024-11-02 12:31:46 +0800

    1

---
+++ src/ch04/GradeStat.java
@@ -0,0 +1,14 @@
+package ch04;
+
+public class GradeStat {
+    private String courseName;
+    private int grades[];
+
+    // construct GradeStat class, creating an instance of GradeStat class and initiating the courseName and grades attributes
+    public GradeStat(String name, int gradesArray[]) {
+        courseName = name;
+        grades = gradesArray;
+    }
+
+
+}
