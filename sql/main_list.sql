c85c1cc92a82f379e5a90c55f643474248ac1610
Zheng_Da <74081090+SirLearning@users.noreply.github.com>
2025-01-04 12:00:02 +0800

    1

--- sql/main_list.sql
+++ sql/main_list.sql
@@ -2,18 +2,13 @@ show databases;
 create database vmap4;
 use vmap4;
 # drop database vmap4;
+
 show engines;
 show variables like "have%";
 show variables like "default_storage_engine";
 set default_storage_engine=MyISAM;
 set default_storage_engine=InnoDB;
 show variables like "%storage_engine%";
-create table main_list(
-    
-);
-
-create database vmap4;
-use vmap4;
 
 create table main_list (
     id int primary key auto_increment,
